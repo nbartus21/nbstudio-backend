cc0df261695ef535732f5efa1137e18439928ddc
nbartus21 <148685008+nbartus21@users.noreply.github.com>
2025-01-26 21:24:40 +0100

    uj index js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 
 // CORS beállítások
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://nbstudio-frontend.onrender.com', 'http://localhost:5174'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
