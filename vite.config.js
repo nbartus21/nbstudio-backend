cbe7b04dfa98d3428c7552992412cc9fcd7a431f
nbartus21 <148685008+nbartus21@users.noreply.github.com>
2025-01-31 05:40:30 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://38.242.208.190:5001',
+        target: 'https://38.242.208.190:5001',
         changeOrigin: true
       }
     }
