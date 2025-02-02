ce9f1d9bfb5c8db540bdf158594a09b25db7701b
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2025-02-02 12:01:12 +0100

    ip back to ip

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/admin.nb-studio.net/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/admin.nb-studio.net/fullchain.pem'),
-    },
     proxy: {
       '/api': {
         target: 'https://admin.nb-studio.net:5001',
-        secure: false,
         changeOrigin: true
       }
     }
