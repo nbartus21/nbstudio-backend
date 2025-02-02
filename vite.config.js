0c853d35b61caf6a7ffd25be19eb21f4a31a9130
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2025-02-02 12:11:33 +0100

    vite https

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    https: {
-      key: fs.readFileSync('/root/ssl/private.key'),
-      cert: fs.readFileSync('/root/ssl/certificate.crt'),
-    },
     proxy: {
       '/api': {
         target: 'https://admin.nb-studio.net:5001',
-        changeOrigin: true,
-        secure: false
+        changeOrigin: true
       }
     }
   }
