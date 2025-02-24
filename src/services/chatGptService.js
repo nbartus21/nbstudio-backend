b8a214b69e5a7bee4f3ae117e20009e8812170c2
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2025-02-24 21:50:25 +0100

    blog es navigacio update

--- src/services/chatGptService.js
+++ src/services/chatGptService.js
@@ -1,57 +1,26 @@
-
 import { deepseekService } from './deepseekService';
 
-// Blog tartalom generálás
+// Módosítsuk az exportokat
 export const generateBlogContent = async (topic, language = 'hu') => {
   return deepseekService.generateBlogContent(topic, language);
 };
 
-// Blog cím generálás
 export const generateTitle = async (topic, language = 'hu') => {
   return deepseekService.generateTitle(topic, language);
 };
 
-// SEO leírás generálás
 export const generateSEODescription = async (content, language = 'hu') => {
   return deepseekService.generateSEODescription(content, language);
 };
 
-// Tartalom fordítás
 export const translateContent = async (content, fromLang = 'hu', toLang = 'en') => {
-    return deepseekService.translateContent(content, fromLang, toLang);
-  };
-
-// SEO javaslatok
-export const generateSEOSuggestions = async (content, language) => {
-  return deepseekService.generateSEOSuggestions(content, language);
+  return deepseekService.translateContent(content, fromLang, toLang);
 };
 
-// Meta tartalom generálás
-export const generateMetaContent = async (content, language) => {
-  return deepseekService.generateMetaContent(content, language);
-};
-
-// Címkék javaslata
-export const suggestTags = async (content, language) => {
-  return deepseekService.suggestTags(content, language);
-};
-
-// Üzenet kategorizálás
-export const categorizeMessage = async (message) => {
-  return deepseekService.categorizeMessage(message);
-};
-
-// Válasz javaslat generálás
-export const generateResponseSuggestion = async (message) => {
-  return deepseekService.generateResponseSuggestion(message);
-};
-
-// Üzenet összefoglaló generálás
-export const generateSummary = async (message) => {
-  return deepseekService.generateSummary(message);
-};
-
-// Projekt elemzés
-export const analyzeProject = async (projectData) => {
-  return deepseekService.analyzeProject(projectData);
+// Ha több exportált függvény is van, explicit export listát is használhatunk
+export {
+  generateBlogContent,
+  generateTitle,
+  generateSEODescription,
+  translateContent
 };
