e24c24e62c1fac864f8a29ea2d63871dd6164a78
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2025-03-03 21:41:00 +0100

    forditas page update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './Navigation';
-import Dashboard from './Dashboard';  // Importáljuk a Dashboard komponenst
+import Dashboard from './Dashboard';
 import BlogAdmin from './BlogAdmin';
 import BlogCreator from './BlogCreator';
 import ContactAdmin from './ContactAdmin';
 import CalculatorAdmin from './CalculatorAdmin';
 import Login from './Login';
-import MagicLogin from './MagicLogin';  // Új Magic Link komponens importálása
+import MagicLogin from './MagicLogin';
 import ProjectManager from './ProjectManager';
 import DomainManager from './domain/DomainManager';
 import InfrastructureManager from './InfrastructureManager';
@@ -15,7 +15,8 @@ import SharedProjectView from './SharedProjectView';
 import InvoiceManager from './InvoiceManager';
 import AccountingManager from './AccountingManager';
 import HostingManager from './HostingManager';
-import ServerMonitoring from './ServerMonitoring'; // Import ServerMonitoring component
+import ServerMonitoring from './ServerMonitoring';
+import TranslationTool from './TranslationTool'; // Új komponens importálása
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
@@ -36,7 +37,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/magic-login" element={<MagicLogin />} />  {/* Új Magic Login route */}
+      <Route path="/magic-login" element={<MagicLogin />} />
       
       <Route
         path="/"
@@ -87,6 +88,14 @@ const App = () => {
         }
       />
       <Route
+        path="/translation"
+        element={
+          <PrivateRoute>
+            <TranslationTool />
+          </PrivateRoute>
+        }
+      />
+      <Route
         path="/projects"
         element={
           <PrivateRoute>
@@ -126,14 +135,14 @@ const App = () => {
           </PrivateRoute>
         }
       />
-            <Route
-  path="/infrastructure/monitoring"
-  element={
-    <PrivateRoute>
-      <ServerMonitoring />
-    </PrivateRoute>
-  }
-/>
+      <Route
+        path="/infrastructure/monitoring"
+        element={
+          <PrivateRoute>
+            <ServerMonitoring />
+          </PrivateRoute>
+        }
+      />
       <Route
         path="/hosting"
         element={
@@ -142,22 +151,6 @@ const App = () => {
           </PrivateRoute>
         }
       />
-      <Route
-  path="/infrastructure/monitoring"
-  element={
-    <PrivateRoute>
-      <ServerMonitoring />
-    </PrivateRoute>
-  }
-/>
-<Route
-  path="/translation"
-  element={
-    <PrivateRoute>
-      <TranslationTool />
-    </PrivateRoute>
-  }
-/>
       <Route path="*" element={<Navigate to="/magic-login" />} />
       <Route path="/shared-project/:token" element={<SharedProjectView />} />
     </Routes>
