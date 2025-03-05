3b4347685d0544763d73fd408e8d8cda9b388be3
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2025-03-05 23:00:10 +0100

    support ticket  delete update uj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,142 +20,150 @@ import TranslationTool from './TranslationTool'; // Új komponens importálása
 import SupportTicketManager from './SupportTicketManager';
 
 const App = () => {
-  const PrivateRoute = ({ children }) => {
-    const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
-    
-    if (!isAuthenticated) {
-      return <Navigate to="/login" />;
-    }
+ const PrivateRoute = ({ children }) => {
+   const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
+   
+   if (!isAuthenticated) {
+     return <Navigate to="/login" />;
+   }
 
-    return (
-      <div>
-        <Navigation />
-        {children}
-      </div>
-    );
-  };
+   return (
+     <div>
+       <Navigation />
+       {children}
+     </div>
+   );
+ };
 
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/magic-login" element={<MagicLogin />} />
-      
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <BlogAdmin />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/blog"
-        element={
-          <PrivateRoute>
-            <BlogAdmin />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/blog/new"
-        element={
-          <PrivateRoute>
-            <BlogCreator />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/contacts"
-        element={
-          <PrivateRoute>
-            <ContactAdmin />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/calculator"
-        element={
-          <PrivateRoute>
-            <CalculatorAdmin />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/translation"
-        element={
-          <PrivateRoute>
-            <TranslationTool />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/projects"
-        element={
-          <PrivateRoute>
-            <ProjectManager />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/domains"
-        element={
-          <PrivateRoute>
-            <DomainManager />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/infrastructure"
-        element={
-          <PrivateRoute>
-            <InfrastructureManager />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/invoices"
-        element={
-          <PrivateRoute>
-            <InvoiceManager />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/accounting"
-        element={
-          <PrivateRoute>
-            <AccountingManager />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/infrastructure/monitoring"
-        element={
-          <PrivateRoute>
-            <ServerMonitoring />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/hosting"
-        element={
-          <PrivateRoute>
-            <HostingManager />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/magic-login" />} />
-      <Route path="/shared-project/:token" element={<SharedProjectView />} />
-    </Routes>
-  );
+ return (
+   <Routes>
+     <Route path="/login" element={<Login />} />
+     <Route path="/magic-login" element={<MagicLogin />} />
+     
+     <Route
+       path="/"
+       element={
+         <PrivateRoute>
+           <BlogAdmin />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/dashboard"
+       element={
+         <PrivateRoute>
+           <Dashboard />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/blog"
+       element={
+         <PrivateRoute>
+           <BlogAdmin />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/blog/new"
+       element={
+         <PrivateRoute>
+           <BlogCreator />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/contacts"
+       element={
+         <PrivateRoute>
+           <ContactAdmin />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/calculator"
+       element={
+         <PrivateRoute>
+           <CalculatorAdmin />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/translation"
+       element={
+         <PrivateRoute>
+           <TranslationTool />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/projects"
+       element={
+         <PrivateRoute>
+           <ProjectManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/domains"
+       element={
+         <PrivateRoute>
+           <DomainManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/infrastructure"
+       element={
+         <PrivateRoute>
+           <InfrastructureManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/invoices"
+       element={
+         <PrivateRoute>
+           <InvoiceManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/accounting"
+       element={
+         <PrivateRoute>
+           <AccountingManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/infrastructure/monitoring"
+       element={
+         <PrivateRoute>
+           <ServerMonitoring />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/hosting"
+       element={
+         <PrivateRoute>
+           <HostingManager />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path="/support"
+       element={
+         <PrivateRoute>
+           <SupportTicketManager />
+         </PrivateRoute>
+       }
+     />
+     <Route path="*" element={<Navigate to="/magic-login" />} />
+     <Route path="/shared-project/:token" element={<SharedProjectView />} />
+   </Routes>
+ );
 };
 
 export default App;
