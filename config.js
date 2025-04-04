2b015e575dc74e039d77ad885eb8c715b8cac5ef
Norbert Bartus <nbartus21@gmail.com>
2025-04-04 12:50:55 +0200

    Karbantartási mód implementálása egyszerű konfigurációs fájllal

---
+++ config.js
@@ -0,0 +1,4 @@
+// API és alkalmazás konfigurációs beállítások
+
+// API alap URL
+export const API_URL = 'https://admin.nb-studio.net:5001/api';
---
+++ maintenanceConfig.js
@@ -0,0 +1,5 @@
+// Karbantartási mód beállítása
+// Ha true, akkor a karbantartási üzenet megjelenik a weboldalon
+// Ha false, akkor nem jelenik meg
+
+export const MAINTENANCE_MODE = false;
