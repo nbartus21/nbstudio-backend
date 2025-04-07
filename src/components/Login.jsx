c4a5ad4fbcb699fc4bd87f43f9cd1daf7d0047fa
Norbert Bartus <nbartus21@gmail.com>
2025-04-07 09:46:52 +0200

    A dashboard továbbfejlesztése és routing módosítások, hogy a login után a dashboard legyen a kezdőoldal

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
       sessionStorage.setItem('token', data.token);
       sessionStorage.setItem('email', credentials.email);
       
-      navigate('/dashboard');
+      navigate('/');
     } catch (error) {
       setError(error.message);
       setLoading(false);
